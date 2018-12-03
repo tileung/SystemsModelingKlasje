9eb4b198090fb3c2fb49a666f61f8f2caf61135a
tileung <tileung@users.noreply.github.com>
2018-12-03 13:26:17 +0100

    Add files via upload

--- little-model-optim.py
+++ little-model-optim.py
@@ -11,22 +11,57 @@
 
 import numpy as np
 #import matplotlib as mpl
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from scipy import linalg, optimize
 #from numpy import poly1d
 
-##Advanced case##
-# number of arrivals and departures (per hour) over 12-hour period.
-arrivals = [1, 1, 0, 1, 1, 0, 1, 1, 2, 1, 0, 1]
-departures = [0, 0, 0, 1, 1, 1, 0, 1, 2, 0, 2, 2]
+####Advanced case##
+### number of arrivals and departures (per hour) over 12-hour period.
+##arrivals = [1, 1, 0, 1, 1, 0, 1, 1, 2, 1, 0, 1]
+##departures = [0, 0, 0, 1, 1, 1, 0, 1, 2, 0, 2, 2]
+##
+### calculate average L and lam.
+##tot_arr = np.cumsum(arrivals)
+##tot_dep = np.cumsum(departures)
+##
+##L = np.mean(tot_arr - tot_dep)
+##lam = np.mean(arrivals)
+##W = L/lam
+##
+##print(W)
 
-# calculate average L and lam.
-tot_arr = np.cumsum(arrivals)
-tot_dep = np.cumsum(departures)
+##Expert case##
 
-L = np.mean(tot_arr - tot_dep)
-lam = np.mean(arrivals)
-W = L/lam
+# set up dictionaries to save keys (simulation #) and values (simulated values)
+lamdict={}
+Ldict={}
+Wdict={}            
 
-print(W)
+##for i in range (1,11):              # set number of simulations
+    ##lam = np.random.poisson(5.0)    # simulate arrival rate by Poisson distribution
+for lam in range(1,11):             # Assume different arrival rates lam.
+    for L in range(10,26):          # Assume L varies in size, from e.g. 10 to 25
+        W = L/float(lam)
+        lamdict.setdefault(lam,[]).append(lam)
+        Ldict.setdefault(lam,[]).append(L)
+        Wdict.setdefault(lam,[]).append(W)
 
+# plot for each L, W for lam.
+for i in r:
+    lamplot = lamdict[2][i]
+    Wplot = Wdict[2][i]
+    print lamplot
+    sys.end(0)
+
+print (lamplot)
+print(Wplot)
+
+plt.plot(Wplot,lamplot)
+plt.ylabel('waiting time')
+plt.xlabel('arrival rate')
+plt.show()
+
+# beds = np.arange(1,21)              # define the possible number of beds (1-20, here)
+
+# ?simulated case: start with zero occupied beds, and assume end with zero.
+# los = 8                 # define average LOS for maternity: approx 8 days, depending on type of delivery, world region: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4783077/
